36c4ba3a3111ca5624df5801f12f8d5cdcfce623
Tiwar02 <salazarstiwar@gmail.com>
2023-03-24 22:50:07 -0500

    Initial DB Config

---
+++ Shopped/Shopped/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopped.Data.Entites;
+
+namespace Shopped.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+                
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
---
+++ Shopped/Shopped/Data/Entites/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopped.Data.Entites
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+
+        public string Name { get; set; }    
+
+    }
+}
